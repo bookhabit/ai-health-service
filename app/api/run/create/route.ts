3e169c7ce41b6e0166feb37e1eb526ce1cc9a713
bookhabit <qnrgoqlt@naver.com>
2024-02-26 01:50:17 +0900

    openai API 연동 assistant,message,run

---
+++ app/api/run/create/route.ts
@@ -0,0 +1,35 @@
+import { NextResponse } from "next/server"
+import OpenAI from "openai"
+
+export async function POST(req:Request){
+    const {threadId,assistantId} = await req.json()
+
+    if(!threadId || !assistantId){
+        return NextResponse.json(
+            {error : "threadId and assistantId are required",success:false},
+            {status:400}
+        )
+    }
+    
+    const openai = new OpenAI()
+
+    try{
+        const run = await openai.beta.threads.runs.create(threadId,{
+            assistant_id:assistantId,
+        })
+
+        console.log('from openai run',run)
+
+        return NextResponse.json(
+            {run,success:true},
+            {status:201}
+        )
+    }catch(error){
+        console.log(error)
+        return NextResponse.json(
+            {error : "Something went wrong",success:false},
+            {status:500}
+        )
+    }
+
+}
