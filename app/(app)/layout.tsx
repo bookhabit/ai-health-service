333aad8db8d85be75897609c39064c98e363d09d
bookhabit <qnrgoqlt@naver.com>
2024-02-26 21:43:45 +0900

    run polling API연결 및 채팅페이지 완성

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,11 +1,12 @@
 "use client"
 
-import { userThreadAtom } from "@/atoms";
+import { assistantAtom, userThreadAtom } from "@/atoms";
 import Navbar from "@/components/Navbar";
-import { UserThread } from "@prisma/client";
+import { Assistant, UserThread } from "@prisma/client";
 import axios from "axios";
 import { useAtom } from "jotai";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
+import toast from "react-hot-toast";
 
 export default function AppRootLayout({
   children,
@@ -15,6 +16,38 @@ export default function AppRootLayout({
   // todo : if the user doesn't have threadId, we make one
   // todo : if the user does have one, we fetch it
   const [,setUserThread] = useAtom(userThreadAtom)
+  const [assistant,setAssistant] = useAtom(assistantAtom)
+  console.log('assistant id',assistant)
+
+  useEffect(()=>{
+    if(assistant) return
+
+    async function getAssistant() {
+      try{
+        const response = await axios.get<{
+          success:boolean;
+          message?:string;
+          assistants:Assistant;
+        }>("/api/assistant")
+
+        console.log('assistant 얻기 response',response)
+
+        if(!response.data.success || !response.data.assistants){
+          console.error(response.data.message ?? "Unknown error") 
+          toast.error("Failed to fetch assistant")
+          setAssistant(null)
+          return;
+        }
+  
+        setAssistant(response.data.assistants)
+      }catch(error){
+        console.log(error)
+        setAssistant(null)
+      }
+    }
+    getAssistant()
+  },[setAssistant])
+
   useEffect(()=>{
     // build be endpoint
     async function getUserThread() {
@@ -43,7 +76,7 @@ export default function AppRootLayout({
   // console.log('userThread',userThread)
 
   return (
-    <div className="flex flex-col w-full h-screen">
+    <div className="flex flex-col w-full h-full">
         <Navbar/>
         {children}    
     </div>
