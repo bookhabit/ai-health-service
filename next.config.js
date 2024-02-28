19774d71d3030e767d08813750a2c4a7e27766b5
bookhabit <qnrgoqlt@naver.com>
2024-02-28 18:29:56 +0900

    fix - next.config.js파일 모듈 require 방식 변경

--- next.config.js
+++ next.config.js
@@ -1,16 +1,15 @@
 /** @type {import('next').NextConfig} */
-import nextPWA from 'next-pwa';
-const withPwa = nextPWA({
-    dest:'public',
-    swSrc:'/public/custom-service-worker.js',
-    register:true,
-    skipWating:true,
-    buildExcludes:[/middleware-manifest.json$/,/app-build-manifest.json$/],
-    disable:process.env.NODE_ENV === "development",
-})
-
-const nextConfig = withPwa({
-    reactStrictMode:true,
-})
-
-module.exports = nextConfig;
+const withPwa = require("next-pwa")({
+    dest: "public",
+    swSrc: "/public/custom-service-worker.js",
+    register: true,
+    skipWaiting: true,
+    buildExcludes: [/middleware-manifest.json$/, /app-build-manifest.json$/],
+    disable: process.env.NODE_ENV === "development",
+  });
+  
+  const nextConfig = withPwa({
+    reactStrictMode: true,
+  });
+  
+  module.exports = nextConfig;
