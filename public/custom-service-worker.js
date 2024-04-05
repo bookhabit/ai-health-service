82127234f54e718fce350a9f685279134fc4786c
bookhabit <qnrgoqlt@naver.com>
2024-04-05 18:30:17 +0900

    서비스워커의 click 이벤트 재설정

--- public/custom-service-worker.js
+++ public/custom-service-worker.js
@@ -6,16 +6,19 @@ self.addEventListener("push", function (event) {
   const title = data || "A new message!";
   const options = {
     body: data.body || "홈메이트에게서 알림이 왔습니다.",
-    icon: "/icons/icon-192x192.png",
-    badge: "/icons/badge-72x72.png",
+    icon: "./icon-192x192.png",
+    badge: "/badge-72x72.png",
   };
-
-  event.waitUntil(self.registration.showNotification(title, options));
+  // event.waitUntil(self.registration.showNotification(title, options));
+console.log('options의 아이콘',options.icon)
+  const notification = new self.Notification(title,options)
+  notification.addEventListener("click",()=>{
+    clients.openWindow("https://homemate-ai.vercel.app/")
+  })
+  
 });
 
-self.addEventListener('notificationclick', function(event) {
-  event.notification.close();
-  event.waitUntil(
-    clients.openWindow(process.env.NEXT_PUBLIC_BASE_URL)
-  );
-});
+// self.addEventListener('notificationclick', function(event) {
+//   self.clients.openWindow("https://homemate-ai.vercel.app/")
+//   event.notification.close();
+// });
